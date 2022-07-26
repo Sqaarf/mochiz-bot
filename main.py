5313afc4b3da97bb795652d260fdc8f0abae90d5
Sqaarf <mrscarf19@gmail.com>
2022-07-26 13:50:50 +0200

    More token handling fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import requests
+import os
 
 import discord
 from discord.ext import commands
@@ -85,4 +86,4 @@ async def echo(ctx, *args):
 async def hey(ctx):
     await ctx.send("Hey motherfuckaaaz :mochiz:")
 
-bot.run("TOKEN")
+bot.run(os.environ("TOKEN"))
