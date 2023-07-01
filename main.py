50b9d766e4ec6a8063f7918e1740672bc376b4c4
zsabbah <zyad.sabbah@neurones.net>
2023-07-02 00:19:04 +0200

    feat(Slash): Added Slash commands

--- main.py
+++ main.py
@@ -4,73 +4,84 @@ import os
 from dotenv import load_dotenv
 
 import discord
+from discord import app_commands
 from discord.ext import commands
 
 load_dotenv()
 
 # Setup the bot with a custom prefix
 # Default prefix is 'm.'
-intents = discord.Intents.default()
-intents.message_content = True
-bot = commands.Bot(command_prefix='m.', intents=intents)
+
+bot = commands.Bot(command_prefix='m.', intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print("Bot is Up and Ready !")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
+
 
 # List of answers in 8ball command
 eightball_answers = ["Myes", "Nope" ,"100% sure", "Kinda", "Not even in your wildest dream", "idk google it ?"]
 # List of possible moves in Rock, Paper & Scissors
 rps_choices = ["Rock", "Paper", "Scissors"]
 
-@bot.command()
-async def answer(ctx):
+@bot.tree.command(name="answer")
+async def answer(interaction: discord.Interaction):
     '''
     Send a random answer to a question
     '''
     choice = random.choice(eightball_answers)
-    await ctx.send(choice)
+    await interaction.response.send_message(choice)
 
 #------------------------------------RANDOM PICS----------------------------------------------------------------------------------
 
 @bot.command()
-async def dog(ctx):
+async def dog(interaction: discord.Interaction):
     '''
     Send a random image of a dog using some-random-api.ml
     '''
     r = requests.get('https://some-random-api.ml/animal/dog')
-    await ctx.send(r.json()["image"])
+    await interaction.response.send_message(r.json()["image"])
 
 @bot.command()
-async def cat(ctx):
+async def cat(interaction: discord.Interaction):
     '''
     Send a random image of a cat using some-random-api.ml
     '''
     r = requests.get('https://some-random-api.ml/animal/cat')
-    await ctx.send(r.json()["image"])
+    await interaction.response.send_message(r.json()["image"])
 
 @bot.command()
-async def raccoon(ctx):
+async def raccoon(interaction: discord.Interaction):
     '''
     Send a random image of a raccoon using some-random-api.ml
     '''
     r = requests.get('https://some-random-api.ml/animal/raccoon')
-    await ctx.send(r.json()["image"])
+    await interaction.response.send_message(r.json()["image"])
 
 @bot.command()
-async def fox(ctx):
+async def fox(interaction: discord.Interaction):
     '''
     Send a random image of a fox using some-random-api.ml
     '''
     r = requests.get('https://some-random-api.ml/animal/fox')
-    await ctx.send(r.json()["image"])
+    await interaction.response.send_message(r.json()["image"])
 
 @bot.command()
-async def birb(ctx):
+async def birb(interaction: discord.Interaction):
     '''
     Send a random image of a bird using some-random-api.ml
     '''
     r = requests.get('https://some-random-api.ml/animal/birb')
-    await ctx.send(r.json()["image"])
+    await interaction.response.send_message(r.json()["image"])
 
-@bot.command()
-async def repeat(ctx, *args):
+@bot.tree.command(name="repeat")
+async def repeat(interaction: discord.Interaction, word: str, coeff: int):
     '''
     Send a given word a number given of times
 
@@ -78,13 +89,11 @@ async def repeat(ctx, *args):
             args[0] (str): The word you want to be repeated
             args[1] (str): The number of times you want thhe word to be repeated
     '''
-    word = args[0]
-    coeff = int(args[1])
     response = " ".join([word for x in range(coeff)])
-    await ctx.send(response)
+    await interaction.response.send_message(response)
 
-@bot.command()
-async def rps(ctx, *args):
+@bot.tree.command(name="rps")
+async def rps(interaction: discord.Interaction, choice: str):
     '''
     A game of Rock, Paper & Scissors
 
@@ -93,53 +102,52 @@ async def rps(ctx, *args):
     '''
     rps_choice = ""
     rps_choice = random.choice(rps_choices)
-    if args[0].lower() == rps_choice.lower():
+    if choice.lower() == rps_choice.lower():
         response = f"A DRAW ? WTF CHEATER >:("
-    elif args[0].lower() == "rock":
+    elif choice.lower() == "rock":
         if rps_choice == "Scissors":
             response = f"{rps_choice} ! Wait wha- YOU STUPID ASS MF I LOST"
         else:
             response = f"{rps_choice} ! Hahaha get rekt loseeer :p"
-    elif args[0].lower() == "scissors":
+    elif choice.lower() == "scissors":
        if rps_choice == "Paper":
            response = f"{rps_choice} ! Wait wha- YOU STUPID ASS MF I LOST"
        else:
            response = f"{rps_choice} ! Hahaha get rekt loseeer :p"
-    elif args[0].lower() == "paper":
+    elif choice.lower() == "paper":
        if rps_choice == "Rock":
            response = f"{rps_choice} ! Wait wha- YOU STUPID ASS MF I LOST"
        else:
            response = f"{rps_choice} ! Hahaha get rekt loseeer :p"
     else:
         response = f"Stop spitting nonsense it's rock, paper or scissors... MMMMMMMM"
-    await ctx.send(response)
+    await interaction.response.send_message(response)
 
-@bot.command()
-async def calc(ctx, *args):
+@bot.tree.command(name="calc")
+async def calc(interaction: discord.Interaction, expression: str):
     '''
     Send the result of a simple mathematical calculus expression
 
         Parameters:
             args[0] (str): Pythonic calculus expression 
     '''
-    op = str(args[0])
-    await ctx.send(str(eval(op)))
+    await interaction.response.send_message(str(eval(expression)))
 
-@bot.command()
-async def echo(ctx, *args):
+@bot.tree.command(name="echo")
+async def echo(interaction: discord.Interaction, text: str):
     '''
     Rewrite a sentence/word
         
         Parameters:
             args (list<str>): The word/sentence you want to send
     '''
-    await ctx.send(" ".join(args))
+    await interaction.response.send_message(text)
 
-@bot.command()
-async def hey(ctx):
+@bot.tree.command(name="hey")
+async def hey(interaction: discord.Interaction):
     '''
     Send a simple introduction
     '''
-    await ctx.send("Hey motherfuckaaaz i'm Mochiz destroyer of worlds youuuu knooow")
+    await interaction.response.send_message("Hey motherfuckaaaz i'm Mochiz destroyer of worlds youuuu knooow")
 
 bot.run(os.environ.get("DISCORD_TOKEN"))
